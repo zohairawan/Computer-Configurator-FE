aa74c48dd6982fd9206db0e40f55fc319530374c
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-15 05:21:51 -0700

    Create ProductPage

---
+++ src/main/java/com/computer_configurator/pages/ProductPage.java
@@ -0,0 +1,11 @@
+package com.computer_configurator.pages;
+
+import com.computer_configurator.pages.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPage extends BasePage {
+
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
+}
