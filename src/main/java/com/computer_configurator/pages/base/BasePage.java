0485bae7d68f13758a1c0e39156974de4b542b65
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-15 03:49:46 -0700

    Create BasePage and add core methods

---
+++ src/main/java/com/computer_configurator/pages/base/BasePage.java
@@ -0,0 +1,37 @@
+package com.computer_configurator.pages.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    private WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void set(By locator, String text) {
+        find(locator).sendKeys(text);
+    }
+
+    public void clearAndSet(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    public void click(By locator) {
+        find(locator).click();
+    }
+
+    public void scrollTo(By locator) {
+        new Actions(driver).scrollToElement(find(locator)).perform();
+    }
+}
