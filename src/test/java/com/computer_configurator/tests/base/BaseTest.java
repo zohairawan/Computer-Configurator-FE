17e8d60e34e79f39aa53f0c03a4530428cf99080
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-15 03:50:14 -0700

    Create BaseTest and add core methods

--- src/test/java/com/computer_configurator/tests/base/BaseTest.java
+++ src/test/java/com/computer_configurator/tests/base/BaseTest.java
@@ -1,4 +1,30 @@
 package com.computer_configurator.tests.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @BeforeMethod
+    public void loadApplication() {
+        driver.get("https://www.staging2.computerconfigurator.com/product-category/all-computers/");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
